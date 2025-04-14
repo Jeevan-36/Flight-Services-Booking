907af12fa2a6b73f7849e152e5b6c0bdcb81fccf
Jeevan-36 <reddyjeevan2006@gmail.com>
2025-04-14 13:09:25 +0530

    Add Booking model and migration for booking management

---
+++ src/migrations/20250414072509-create-booking.js
@@ -0,0 +1,44 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Bookings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull:false
+      },
+      status: {
+        type: Sequelize.ENUM,
+        values:['pending','initiated','cancelled','booked'],
+       defaultValue:'initiated',
+       allowNull:false
+      },
+      totalCost: {
+        type: Sequelize.INTEGER,
+        allowNull:false
+      },
+      noOfSeats:{
+        type:Sequelize.INTEGER,
+        allowNull:false,
+        defaultValue:1
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Bookings');
+  }
+};
